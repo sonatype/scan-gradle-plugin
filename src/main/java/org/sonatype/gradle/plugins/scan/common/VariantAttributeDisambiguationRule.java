8271f4e0868920e68366c9cbd12c4a0a6328064e
Eduard Tita <etita@sonatype.com>
2026-08-24 15:39:42 -0400

    Update license headers to reflect current copyright and attribution policy

--- src/main/java/org/sonatype/gradle/plugins/scan/common/VariantAttributeDisambiguationRule.java
+++ src/main/java/org/sonatype/gradle/plugins/scan/common/VariantAttributeDisambiguationRule.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016-present Sonatype, Inc. All rights reserved.
+ *
+ * Includes the third-party code listed at https://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
 package org.sonatype.gradle.plugins.scan.common;
 
 import javax.inject.Inject;
